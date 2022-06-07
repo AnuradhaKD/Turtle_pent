2b4c912e2c4fe4507ede22cc0216eddca80b3bed
D.M.Anuradha Kumara Dissanayake <66989020+AnuradhaKD@users.noreply.github.com>
2022-06-08 00:53:44 +0530

    Add files via upload

---
+++ Turtle.py
@@ -0,0 +1,10 @@
+import turtle
+turtle.bgcolor("black")
+colors=['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'purple']
+turtle.speed(0.5)
+for i in range(288):
+	turtle.pencolor(colors[i%7])
+	turtle.pensize(i/88+1)
+	turtle.forward(i)
+	turtle.left(59)
+turtle.done()
